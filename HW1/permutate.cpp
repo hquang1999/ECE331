02aec4a36334312d05d0aa002e8981e5fc404955
Hieu <hquang@unm.edu>
2023-01-23 01:12:48 -0700

    Update: Ci added for comparisons

--- HW1/permutate.cpp
+++ HW1/permutate.cpp
@@ -11,36 +11,47 @@ void printVec(std::vector<int> &x) {
 }
 
 // Insertion Sort
-void insertionSort(std::vector<int> &x, int C) {
+void insertionSort(std::vector<int> &x, int &C) {
     int max = x.size();
     for (int i = 1; i < max; i++) {
         int key = x[i];
         int j = i - 1;
 
         while ((j >= 0) && (x[j] > key)) {
-            x[j + 1] = x[j];
-            j--;
+            C++;
+            if (x[j] > key) {
+                x[j + 1] = x[j];
+                j--;
+                C++;
+            }
+            else {
+                break;
+            }
         }
         x[j + 1] = key; 
     } 
 }
 
 // Main function that does permuation of given vector 
-void permutate(std::vector<int> &main, int C) {
+void permutate(std::vector<int> &main) {
+    int i = 0;
     do {
         std::vector<int> x = main;
+        int Ci = 0;
         std::cout << "Initial" << std::endl;
         printVec(x);
-        insertionSort(x,0);
+        insertionSort(x,Ci);
         std::cout << "Sorted" << std::endl;
         printVec(x);
+        std::cout << "C" << i << " = " << Ci << std::endl;
+        i++;
         std::cout << std::endl;
     } while(std::next_permutation(main.begin(),main.end()));
 }
 
 int main () {
     std::vector<int> x = {1,2,3,4}; 
-    permutate(x,0);
+    permutate(x);
 
     return 0;
 }
